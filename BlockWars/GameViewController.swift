29ecdb8b9aa8af4a60b84f64c160dc0a82aa91ba
Carmen <carmenjyuen@gmail.com>
2014-11-30 21:22:25 -0800

    curly brackets missplaced

--- BlockWars/GameViewController.swift
+++ BlockWars/GameViewController.swift
@@ -14,7 +14,8 @@ import SpriteKit
 class GameViewController: UIViewController {
 
     var scene: GameScene!
-    
+    var swiftris:Swiftris!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,9 +27,22 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
+        scene.tick = didTick
+        
+        swiftris = Swiftris()
+        swiftris.beginGame()
+        
         //Present the scene
         skView.presentScene(scene)
         
+        scene.addPreviewShapeToScene(swiftris.nextShape!) {
+            self.swiftris.nextShape?.moveTo(StartingColumn, row: StartingRow)
+            self.scene.movePreviewShape(self.swiftris.nextShape!) {
+                let nextShapes = self.swiftris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeToScene(nextShapes.nextShape!) {}
+            }
+        }
 
         
     }
@@ -37,5 +51,10 @@ class GameViewController: UIViewController {
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func didTick() {
+        swiftris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(swiftris.fallingShape!, completion: {})
     }
 }
